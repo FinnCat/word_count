a8a4375119a0c5b1fa3ddbca15996c0dd3b9b603
Ari Aalto <ari.h.aalto@gmail.com>
2018-04-17 00:19:46 +0300

    Added static settings

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'count',
+    'jobs',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
