bea3c811a87660415ad9cbf0de6855c75ed2c2f7
Ari Aalto <ari.h.aalto@gmail.com>
2018-04-22 23:30:11 +0300

    Boostrap code added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import list_blogs
+from .views import list_blogs, home_blog
 
 urlpatterns = [
     path('', list_blogs, name='blog_list'),
+    path('home/', home_blog, name='blog_home'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import Blog
+
 # Create your views here.
 def list_blogs(request):
     return HttpResponse('Tämä on list_blog -view funktio, taustalla hyrrää PostgreSQL 10')
+
+
+def home_blog(request):
+    blogs = Blog.objects
+    return render(request, 'base.html', {'blogs':blogs})
